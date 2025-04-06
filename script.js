ad5a377860370a3569e476cfe3ff74c681fe4c71
gilmarmourajr <juzinho-gilmar@hotmail.com>
2025-04-06 16:45:44 -0300

    Add body, with addBooks function

--- script.js
+++ script.js
@@ -11,9 +11,46 @@ function addBookToLibrary(title, author, pages) {
     bookArray.push(book);
 }
 
-const bookArray = [];
+function addBooksToDisplay() {
+    clearDisplay();
+    
+    for (let book of bookArray) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("book");
 
-addBookToLibrary("Hobbit", "Tolkien", "295");
-addBookToLibrary("Bible", "God", "A lot");
-console.log(bookArray[0]);
-console.log(bookArray[1]);
+        const dataDiv = document.createElement("div");
+        const pTitle = document.createElement("p");
+        pTitle.textContent = `Title: ${book.title}`
+        const pAuthor = document.createElement("p");
+        pAuthor.textContent = `Author: ${book.author}`;
+        const pPages = document.createElement("p");
+        pPages.textContent = `Pages: ${book.pages}`;
+        dataDiv.append(pTitle, pAuthor, pPages);
+
+        const btnDiv = document.createElement("div");
+        btnDiv.classList.add("btns");
+        const statusBtn = document.createElement("button");
+        statusBtn.classList.add("btn", "status");
+        statusBtn.textContent = "Not read";
+        const editBtn = document.createElement("button");
+        editBtn.classList.add("btn");
+        editBtn.textContent = "Edit";
+        const delBtn = document.createElement("button");
+        delBtn.classList.add("btn");
+        delBtn.textContent = "Delete";
+        btnDiv.append(statusBtn, editBtn, delBtn);
+
+        newDiv.append(dataDiv, btnDiv);
+        bookDiv.appendChild(newDiv);
+    }
+}
+
+function clearDisplay() {
+    bookDiv.textContent = '';
+}
+
+const bookArray = [];
+const bookDiv = document.getElementById("books");
+// addBookToLibrary("Hobbit", "Tolkien", "295");
+// addBookToLibrary("Bible", "God", "A lot");
+// addBooksToDisplay();
